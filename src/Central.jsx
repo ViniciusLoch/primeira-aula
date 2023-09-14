546e4f79cf8fc12e11fad524b72ce50d7b488f07
Vinicius loch <viniciusdrloch@gmail.com>
2023-09-14 20:53:30 -0300

    Primeiro exercicio

---
+++ src/Central.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+import Primeiro from './componentes/Primeiro'
+
+function Central() {
+    return (
+        <Primeiro/>
+    )
+
+}
+
+export default Central;
---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+function Primeiro() {
+    return (
+    <Text>
+        primeiro exercicio
+    </Text>
+    )       
+
+}
+
+export default Primeiro;
